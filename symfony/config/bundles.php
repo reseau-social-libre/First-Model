53d1cfc4a2dc6ca47ad0c6b942f8bd42b244c0e2
David Souvignon <david.souvignon@gmail.com>
2019-02-24 20:41:52 +0100

    Using StofDoctrineExtensionBundle to enable Doctrine extensions

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -17,4 +17,5 @@ return [
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
